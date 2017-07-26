e049f15b138a2d4e174b5db8dbe296f3169623d3
Gökcan Değirmenci <gokcan.degirmenci@ug.bilkent.edu.tr>
2017-07-26 17:31:22 +0300

    ll

--- src/main/java/com/gokcan/springboot/Config/WebMVCConfig.java
+++ src/main/java/com/gokcan/springboot/Config/WebMVCConfig.java
@@ -68,7 +68,7 @@ public class WebMVCConfig extends WebMvcConfigurerAdapter {
     public ViewResolver internalResourceViewResolver() {
         InternalResourceViewResolver bean = new InternalResourceViewResolver();
         bean.setViewClass(JstlView.class);
-        bean.setPrefix("/");
+        bean.setPrefix("/views/");
         bean.setSuffix(".jsp");
         return bean;
     }
